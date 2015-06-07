4913b70d7f0e73c64727d33443df8d44862717f4
Shaaheen Sacoor <sacoorshaheen@gmail.com>
2015-06-07 22:09:05 +0200

    Completed Controller and button functionalities Main logic of game finished. Design will be worked on next.

--- battleship/battleship.js
+++ battleship/battleship.js
@@ -21,7 +21,7 @@ var view = {
 
 var model = {
     boardSize: 6,
-    numShips: 3,
+    numShips: 1,
     shipLength:3,
     shipsSunk:0,
 
@@ -31,7 +31,8 @@ var model = {
         for (var i=0;i <this.numShips;i++){
             var ship = this.ships[i];
             var ind = ship.locations.indexOf(guess);
-            if (ind !=-1){
+
+            if (ind > -1){
                 ship.hits[ind] = "hit";
                 view.diplayMessage("Hit!");
                 view.displayHit(guess);
@@ -48,7 +49,7 @@ var model = {
     },
 
     checkSunk: function(ship){
-        for (var i=0;i<ship.shipLength;i++){
+        for (var i=0;i<this.shipLength;i++){
             if (ship.hits[i] == ""){
                 return false;
             }
@@ -57,5 +58,55 @@ var model = {
     }
 };
 
+var controller = {
+    guesses: 0,
 
-model.fire("A6");
+    processGuess: function(guess){
+        if (this.validate(guess)){
+            this.guesses++;
+            var hit = model.fire(guess);
+            if (hit && (model.shipsSunk === model.numShips)){
+                view.diplayMessage("All ships sunk");
+            }
+        }
+        else{
+            alert("Please enter valid coordinates");
+        }
+    },
+
+    validate: function(guess){
+        var letters = ["A","B","C","D","E"];
+        if (guess.length == 2){
+            if (letters.indexOf(guess.charAt(0)) !=-1){
+                var  col = guess.charAt(1);
+                if ((col > 0) && (col <= 6)){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+};
+function init(){
+    var fireButton = document.getElementById("fire");
+    fireButton.onclick = handleFireButton;
+    var guessIn = document.getElementById("guess");
+    guessIn.onkeypress = handleKeyPress;
+}
+
+function handleFireButton(){
+    var guessIn = document.getElementById("guess");
+    var guess = guessIn.value;
+    controller.processGuess(guess);
+    guess.value= "";
+}
+
+function handleKeyPress(e){
+    var fireButton = document.getElementById("fire");
+    if (e.keyCode === 13){
+        fireButton.click();
+        return false;
+    }
+}
+
+window.onload = init;
