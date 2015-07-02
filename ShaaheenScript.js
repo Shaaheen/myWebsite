0416148fda5451f0e68496173b8804924b7dd0d2
Shaaheen Sacoor <sacoorshaheen@gmail.com>
2015-07-02 15:19:13 +0200

    Current background is now stored so each page is set to chosen background

--- ShaaheenScript.js
+++ ShaaheenScript.js
@@ -4,15 +4,34 @@
  */
 
 var currWallPaper = 0;
+var child = false;
 var backgrounds = ["'images/outer.jpg'","'images/space.jpg'","'images/space2.jpg'","'images/space3.jpg'","'images/space4.jpg'"];
 var numOfWallpapers = backgrounds.length - 1;
+
 function init(){
+    if (localStorage.getItem("currBack", currWallPaper) == NaN){
+        localStorage.setItem("currBack", currWallPaper);
+    }
+    console.log("Gets here");
+    if (document.getElementById("Intro") == null){
+        console.log("child");
+        backgrounds = ["'../images/outer.jpg'","'../images/space.jpg'","'../images/space2.jpg'","'../images/space3.jpg'","'../images/space4.jpg'"];
+    }
+    else{
+        backgrounds = ["'images/outer.jpg'","'images/space.jpg'","'images/space2.jpg'","'images/space3.jpg'","'images/space4.jpg'"];
+        console.log("parent");
+    }
+
+    var currWalll = parseInt(localStorage.getItem("currBack"));
+    console.log(currWalll);
+    var newBack = backgrounds[currWalll];
+    document.body.style.backgroundImage = "url(" + newBack + ")";
 
     //give the buttons their events
     var leftArrow = document.getElementById('leftArrow');
     var rightArrow = document.getElementById('rightArrow');
     leftArrow.addEventListener('click',leftArrw,false);
-    rightArrow.addEventListener('click',rightArrw,false);
+    rightArrow.addEventListener('click',rightArrw,false);4
 
     //For arrows to do the same as buttons
     document.onkeydown = function(e){
@@ -34,6 +53,7 @@ function leftArrw(){
     }
     var newBack = backgrounds[currWallPaper];
     document.body.style.backgroundImage = "url(" + newBack + ")";
+    localStorage.setItem("currBack", currWallPaper);
 }
 
 function rightArrw(){
@@ -45,6 +65,7 @@ function rightArrw(){
     }
     var newBack = backgrounds[currWallPaper];
     document.body.style.backgroundImage = "url(" + newBack + ")";
+    localStorage.setItem("currBack", currWallPaper);
 }
 
 window.onload = init;
