b13abee1bc8f9cb0543c4a5827623fec567a46af
Shaaheen Sacoor <sacoorshaheen@gmail.com>
2016-01-08 17:44:26 +0200

    Fixed PHP code to work with new host

--- CheckList/CheckListDB.js
+++ CheckList/CheckListDB.js
@@ -2,9 +2,11 @@
  * Created by Shaaheen on 12/7/2015.
  */
 
+var myFirebaseRef = new Firebase("https://boiling-torch-6214.firebaseio.com/CheckList");
+var keys = [];
+var result;
 function initList(){
     console.log("Got to the list init()")
-    var myFirebaseRef = new Firebase("https://boiling-torch-6214.firebaseio.com/CheckList");
     /*
      var usersRef = myFirebaseRef.child("users");
      usersRef.set({
@@ -28,24 +30,68 @@ function initList(){
     */
 
     var listDisplay = document.getElementById("CheckList");
-    var itemsString = "";
     var count = 0;
+    result = document.getElementById("Result");
     // Retrieve new posts as they are added to our database
     myFirebaseRef.on("child_added", function(snapshot, prevChildKey) {
-        count++;
-        var newPost = snapshot.val();
-        console.log("Author: " + newPost.item);
-        itemsString = itemsString + "Item " + count + ": " + newPost.item + "<br/>";
-        console.log("Previous Post ID: " + prevChildKey);
-    });
-    console.log("Here");
 
-    myFirebaseRef.once("value", function(data) {
-        var allData = data.val();
-        console.log("All data" + allData);
-        console.log(itemsString);
-        listDisplay.innerHTML = itemsString;
+        var newPost = snapshot.val();
+        console.log("Value: " + snapshot.key());
+        console.log("Previous Post ID: " + prevChildKey);
+        keys.push([newPost.item,snapshot.key()]);
+        console.log("Key:" + keys[count-1]);
+        listDisplay.innerHTML = listDisplay.innerHTML + "Item " + (count+1)
+            + ": " + newPost.item + "     <input type='button' value='X'  id='" + count + "' onclick='removeItem(this.id)'>"
+            + "<br/>";
+        count++;
+        result.innerHTML = "Added Item: ' " + newPost.item  + " '";
     });
+
+    myFirebaseRef.on("child_removed", function(snapshot) {
+        result.innerHTML = "Removed Item: ' " + snapshot.val().item+ " ' ";
+        count = 0;
+        myFirebaseRef.once("value", function(snap) {
+            listDisplay.innerHTML = "";
+            keys = [];
+            snap.forEach(function(data) {
+                var newPost = data.val();
+                keys.push([newPost.item,data.key()]);
+                listDisplay.innerHTML = listDisplay.innerHTML + "Item " + (count+1)
+                    + ": " + newPost.item + "     <input type='button' value='X'  id='" + count + "' onclick='removeItem(this.id)'>"
+                    + "<br/>";
+                count++;
+            });
+            console.log(keys);
+        });
+    });
+    myFirebaseRef.once("value", function(snap) {
+        result.innerHTML = "Done Loading.";
+        snap.forEach(function(data) {
+            console.log("The " + data.key() + " dinosaur's score is " + data.val().item);
+        });
+    });
+
+}
+
+//Function to add a new item to the check list
+function addNewItem(){
+    var newItemRef = myFirebaseRef.push();
+    var inputVal = document.getElementById("newItem");
+    newItemRef.set({
+        item: inputVal.value,
+    });
+
+    inputVal.value = "";
+}
+
+function removeItem(clicked_buttonID){
+        var removeKey = keys[clicked_buttonID][1];
+        console.log(removeKey);
+        var removeRef = new Firebase("https://boiling-torch-6214.firebaseio.com/CheckList/" + removeKey);
+        removeRef.remove()
+
+    //console.log("Remove : " + removeRef);
+    //console.log(clicked_buttonID);
 }
 
 window.addEventListener('load',initList,false);
--- contact/contact.php
+++ contact/contact.php
@@ -1,27 +1,28 @@
 <?php
-require ("/home/u899385406//public_html/PHPMailer-master/PHPMailerAutoload.php");
-
 /**
  * Created by PhpStorm.
  * User: Shaaheen
  * Date: 7/3/2015
  * Time: 11:51 PM
- */
-
+ *
 error_reporting(-1);
 ini_set('display_errors', 'On');
 set_error_handler("var_dump");
+ */
 
-define('DB_NAME',"u899385406_main");
-define('DB_USER','u899385406_scr');
-define('DB_PASSWORD','Gigabyte1!');
-define('DB_HOST',"mysql.freehostingnoads.net");
+define('DB_NAME','bdfcmjcz_contactDB');
+define('DB_USER','bdfcmjcz_admin');
+define('DB_PASSWORD','PasswordHere!');
+define('DB_HOST','localhost');
 
 //connect to host database server
 $link2 = mysql_connect(DB_HOST,DB_USER,DB_PASSWORD);
 
 if (!$link2){
     die('Could not connect...');
+}
+else{
+    //echo("\r\n Connected");
 }
 
 //select database
@@ -39,19 +40,20 @@ $message = $_POST["message"];
 //Insert data into database
 $sql = "INSERT INTO Contact (Name,Email,Message) VALUES ('$name','$email','$message')";
 
+//echo("\r\n Inserted");
+
 if (!mysql_query($sql)){
     die("Error" . mysql_error());
 }
 
 mysql_close();
 
+
 //email details
 $my_email = 'SCRSHA001@myuct.ac.za';
 $subject = "Website Message from :" . $name;
 
-echo $my_email;
-echo "$subject";
-echo $message;
+
 $headers = array("From: iam@shaaheen.me",
     "Reply-To: " . $email,
     "X-Mailer: PHP/" . PHP_VERSION
@@ -59,59 +61,15 @@ $headers = array("From: iam@shaaheen.me",
 $headers = implode("\r\n", $headers);
 
 //send email to me
-if(mail('iam@shaaheen.me',"First","Test Msg",$headers)){
-    mail("sacoorshaheen@gmail.com","Sub","Message Here");
-    mail("SCRSHA001@myuct.ac.za","bdcd","vnkj");
-    echo "Sent";
-    echo "\r\n";
-    echo $headers;
+if(mail("SCRSHA001@myuct.ac.za",$subject,$message)){
+    //echo("Sent");
 }
 else{
     echo "Failed to Send";
 }
-if (imap_mail('iam@shaaheen.me',$subject,$message)){
-    echo "Imap Success";
-}
-else{
-    echo "Imap Failed";
-}
-/*
-$mail = new PHPMailer;
 
-//$mail->SMTPDebug = 3;                               // Enable verbose debug output
-
-$mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->Host = 'mx1.freehostingnoads.net';  // Specify main and backup SMTP servers
-$mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'iam@shaaheen.me';                 // SMTP username
-$mail->Password = 'Gigabyte1!';                           // SMTP password
-$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
-$mail->Port = 25;                                    // TCP port to connect to
-
-$mail->From = 'iam@shaaheen.me';
-$mail->FromName = 'Shaaheen Sacoor';
-//$mail->addAddress('sacoorshaheen@gmail.com');     // Add a recipient
-//$mail->addAddress('SCRSHA001@myuct.ac.za');               // Name is optional
-$mail->addAddress('iam@shaaheen.me');               // Name is optional
-//$mail->addReplyTo('GHSOGA@gmail.com', 'Information');
-
-$mail->isHTML(true);                                  // Set email format to HTML
-
-$mail->Subject = 'Here is the subject';
-$mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
-
-if(!$mail->send()) {
-    echo 'Message could not be sent.';
-    echo 'Mailer Error: ' . $mail->ErrorInfo;
-} else {
-    echo 'Message has been sent';
-}
-*/
 //Redirect to Contact page
 header( 'Location: http://shaaheen.me/contact/contact.html' ) ;
 
 exit();
-
-
 //action="contact.php" method="post"
